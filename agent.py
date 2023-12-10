7d4206e2ed57c2caef55e2e4fbdd5ed2e6e1ac8c
fwolter21 <fwolter1@babson.edu>
2023-12-09 22:24:29 -0500

    updated code plus new api key old one was dispabled

--- agent.py
+++ agent.py
@@ -21,6 +21,19 @@ import plotly.express as px
 
 
 def agent_function(file_path, user_input):
+    '''
+    Interacts with an agent to process user input and return a response.
+
+    This function creates an agent instance with ChatOpenAI. It formats the user's input, sends it to the agent, and returns
+    the agent's response.
+
+    Parameters:
+    - file_path (str): The file path of the CSV file being analyzed.
+    - user_input (str): The user's input or query to be processed by the agent.
+
+    Returns:
+    - dict: The response from the agent, or None if an exception occurs.
+    '''
     # Create an agent instance
     agent = create_csv_agent(
         ChatOpenAI(temperature=0, model="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY),
@@ -46,6 +59,18 @@ def agent_function(file_path, user_input):
 
 
 def answers_from_content(response): 
+    '''Processes the response from the agent and displays the results in the Streamlit app.
+
+    This function handles different types of responses such as textual answers, bar charts,
+    tables, line plots, and scatter plots. Depending on the response type, it displays
+    the appropriate content or visualization in the Streamlit app.
+
+    Parameters:
+    - response (dict): The response dictionary from the agent_function.
+
+    Returns:
+    - None: This function does not return a value but displays results in the Streamlit app.
+    '''
     if 'answer' in response: 
         st.write(response['answer'])
     if 'bar' in response: 
@@ -73,7 +98,7 @@ def answers_from_content(response):
 
 def extract_code_from_response(response):
     '''This function takes pyhton code from a string of repsosne. It was developed as I was ecountering issues 
-   making the app function. The code on it was written by chatgbt as I do not fully undertaand how it is doing its function '''
+   making the app function. The code on it was written by chatgbt below as I was struggling to properly account for the variations in user input as well as properly execuitng the code for visualizations'''
     
     code_pattern = r"```python(.*?)```"
     match = re.search(code_pattern, response, re.DOTALL)
@@ -86,6 +111,22 @@ def extract_code_from_response(response):
 
 
 def main():
+    """
+    Main function for the Streamlit app to analyze CSV files.
+
+    This function sets up the Streamlit app layout and handles user interactions.
+    It allows users to upload a CSV file, input a query, and then processes this query
+    using the agent_function. It also handles the visualization of responses, including
+    executing any Python code returned by the agent.
+
+    Steps:
+    - Sets up the Streamlit page configuration and header.
+    - Provides instructions on how to use the app in the sidebar.
+    - Handles CSV file upload and reads the file into a Pandas DataFrame.
+    - Takes user input as a query and processes it using the agent_function.
+    - Executes any returned Python code for visualization and displays it.
+    - Handles and displays errors if they occur during code execution.
+    """
     st.set_page_config(page_title='Ask your CSV')
     st.header('Ask your CSV')
     with st.sidebar:
